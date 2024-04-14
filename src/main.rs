3dc7093f127cee4627eed84b48a797e5cc056367
Rhys <rhys.davies901@gmail.com>
2024-04-13 19:37:38 -0700

    Updated dependencies to get the proj to work, and imports

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-use axum::{
-    routing::get,
-    http::StatusCode,
-    Router,
-    extract::Query,
-    response::IntoResponse,
-};
+use axum::{extract::Query, http::StatusCode, response::IntoResponse, routing::get, Router};
 use serde::Deserialize;
+use sqlx::postgres::{PgPool, PgPoolOptions};
+#[derive(Clone)]
+pub struct AppState {
+    db: PgPool
+}
+
+impl AppState {
+    fn new(db: PgPool) -> Self {
+        Self { db }
+    }
+}
 
 #[derive(Deserialize)]
 struct GreetingParams {
     name: String,
-    fuck_boi: String
+    fuck_boi: String,
 }
 
 async fn hello_world(Query(params): Query<GreetingParams>) -> impl IntoResponse {
@@ -18,13 +23,15 @@ async fn hello_world(Query(params): Query<GreetingParams>) -> impl IntoResponse 
     (StatusCode::OK, greeting)
 }
 
-async fn holdens_fuckboi_function() -> impl IntoResponse {
-    (StatusCode::OK, "Fart in my mouth please!")
-}
-
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/hello", get(hello_world)).route("/anus", get(holdens_fuckboi_function));
+async fn main(#[shuttle_shared_db::Postgres] conn_string: String) -> shuttle_axum::ShuttleAxum {
+    let db = PgPoolOptions::new().connect(&conn_string).await.expect("Fucked up!!!");
+
+    let state = AppState::new(db);
+
+    let router = Router::new()
+        .route("/hello", get(hello_world))
+        .with_state(state);
 
     Ok(router.into())
 }
